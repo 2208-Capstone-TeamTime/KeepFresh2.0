059b2e0df443922415ceaeb7d3f5c88d2bf36741
Guang Qu <qguang95@gmail.com>
2022-12-08 12:10:46 -0500

    Main Merge and Receipt Routing w/ delete function

--- server/controllers/receiptController.js
+++ server/controllers/receiptController.js
@@ -1,2 +1,17 @@
 const Receipts = require("../models/Receipts")
-const Products = require("../models/Products")
+
+const Receipts = require("../models/Receipts")
+
+exports.findAllReceipts = (req, res) => {
+
+    Receipts.find({})
+   .then(data => {
+     res.send(data);
+   })
+   .catch(err => {
+     res.status(500).send({
+       message:
+         err.message || "Some error occurred while retrieving receipts."
+     });
+   });
+};
