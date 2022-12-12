68b982e4636ae959a9d552d33b98a993111a23d5
gowithzflow <zskateray@gmail.com>
2022-12-12 14:25:23 -0500

    Made adjsutments to  Expirations and History Component

--- src/components/History.js
+++ src/components/History.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectPast, findReceiptsbyUserId } from '../reducers/receiptsSlice';
 import { currentUser } from '../reducers/userSlice';
@@ -6,23 +6,32 @@ import { currentUser } from '../reducers/userSlice';
 
 const History = () => {
   const pastReceipts = useSelector(selectPast);
-  const dispatch = useDispatch();
   const user = useSelector(currentUser)
-
+  const dispatch = useDispatch();
+ 
   useEffect(() => {
     dispatch(findReceiptsbyUserId(user))
-    console.log('past:', pastReceipts, user);
+    
+    console.log('past:', pastReceipts, user.displayName);
 },[])
   return (
     <div>
+
       <h1>Expiration History</h1>
-      {pastReceipts.map((item) => (<>
-        <div key={item._id}>
-          <h4>{item.name}</h4>
-          <p>{item.fridgeExp}</p>
-          <p>{item.freezerExp}</p>
-        </div>
-      </>))}
+      {pastReceipts.map((receipt,idx) => (<>
+      <div>
+        {idx === 0? '':
+          <h2>Receipt #{idx}</h2>}
+        {receipt.products.map((item) => (<>
+          <div key={item._id}>
+            <h4>{item.name}</h4>
+            <p>{item.fridgeExp}</p>
+            <p>{item.freezerExp}</p>
+          </div>
+        </>))}
+        </div></>)
+      )}
+
     </div>
   );
 }
